a14f8829cb842124ac49f22565a5a218b70b3ce4
FreeBird-spectre <sethabir699@gmail.com>
2025-08-12 23:02:26 +0530

    Create Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,25 @@
+// Abir Seth
+// 24070123003
+// ENTC A1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int *aptr = &a;
+    cout << a << endl;
+    cout << aptr << endl;
+    cout << *aptr << endl;
+    cout << &a << endl;
+
+    return 0;
+}
+
+10
+0x7ffc8d4fdcb4
+10
+0x7ffc8d4fdcb4
+
+
+=== Code Execution Successful ===
